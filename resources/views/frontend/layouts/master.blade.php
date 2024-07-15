115d5364e3936cbafe0665fb37b188fdf44b2b35
Joshua20b <egbonjoshua2020@gmail.com>
2024-07-14 19:56:54 -0700

    commit

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>{{ $title }} || {{ config('app.name') }} </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.html">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Place favicon.png in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="/frontend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/frontend/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/frontend/css/animate.min.css">
+    <link rel="stylesheet" href="/frontend/css/nice-select.css">
+    <link rel="stylesheet" href="/frontend/css/magnific-popup.css">
+    <link rel="stylesheet" href="/frontend/css/all.min.css">
+    <link rel="stylesheet" href="/frontend/css/meanmenu.css">
+    <link rel="stylesheet" href="/frontend/css/slick.css">
+    <link rel="stylesheet" href="/frontend/css/default.css">
+    <link rel="stylesheet" href="/frontend/css/style.css">
+    <link rel="stylesheet" href="/frontend/css/responsive.css">
+</head>
+
+<body>
+
+    <!-- header begin -->
+    @include('frontend.layouts.header')
+    <!-- header end -->
+
+    <main>
+        @yield('content')
+        @include('frontend.layouts.footer')
+    </main>
+
+    <!-- JS here -->
+    <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="/frontend/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="/frontend/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="/frontend/js/popper.min.js"></script>
+    <script src="/frontend/js/bootstrap.min.js"></script>
+    <script src="/frontend/js/owl.carousel.min.js"></script>
+    <script src="/frontend/js/isotope.pkgd.min.js"></script>
+    <script src="/frontend/js/one-page-nav-min.js"></script>
+    <script src="/frontend/js/slick.min.js"></script>
+    <script src="/frontend/js/ajax-form.js"></script>
+    <script src="/frontend/js/wow.min.js"></script>
+    <script src="/frontend/js/jquery.nice-select.min.js"></script>
+    <script src="/frontend/js/jquery.scrollUp.min.js"></script>
+    <script src="/frontend/js/jquery.meanmenu.min.js"></script>
+    <script src="/frontend/js/jquery.counterup.min.js"></script>
+    <script src="/frontend/js/waypoints.min.js"></script>
+    <script src="/frontend/js/imagesloaded.pkgd.min.js"></script>
+    <script src="/frontend/js/jquery.magnific-popup.min.js"></script>
+    <script src="/frontend/js/plugins.js"></script>
+    <script src="/frontend/js/main.js"></script>
+</body>
+
+
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\PagesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Pages route 
+Route::controller(PagesController::class)->group(function (){});
 
 require __DIR__.'/auth.php';
